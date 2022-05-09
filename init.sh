11357af0e7743005fb9340e88c59c7e154058166
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-05-09 14:08:47 -0300

    improve

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 echo 'SLEEP'
 echo sleep 90s
-echo 'START'
-/opt/mssql-tools/bin/sqlcmd -S mssql -U SA -P "${SA_PASSWORD}" -Q "CREATE DATABASE ${DATABASE_NAME}"
+echo "START ${DATABASE_NAME} ${SA_PASSWORD}"
+/opt/mssql-tools/bin/sqlcmd -S mssql -U SA -P "${SA_PASSWORD}" -Q "
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${DATABASE_NAME}')
+  BEGIN
+    CREATE DATABASE ${DATABASE_NAME}
+  END
+"
 echo 'END'
